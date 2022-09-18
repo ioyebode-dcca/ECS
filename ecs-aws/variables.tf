7bd69d5751eaa2d843516f312d833f799b1df2be
root <root@ip-172-31-89-2.ec2.internal>
2022-09-18 01:34:51 +0000

    AWS-ECS

---
+++ ecs-aws/variables.tf
@@ -0,0 +1,14 @@
+variable "aws_access_key" {
+  description = "The AWS access key."
+  default = "AKIAUNUCME4AR2KJZWIB"
+}
+
+variable "aws_secret_key" {
+  description = "The AWS secret key."
+  default = "6NGk31L0eFmwNHljl/1AYrBWnUVeGaTKuEeIyg6v"
+}
+
+variable "region" {
+  description = "The AWS region to create resources in."
+  default = "us-east-2"
+}
